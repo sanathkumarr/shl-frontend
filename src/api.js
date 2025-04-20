066ef8c6a2ecd6eb0a0fb0c3ef5b4231a03e69a9
sanathkumarr <ravadagundisanath@gmail.com>
2025-04-20 22:37:53 +0530

    Frontend ready  Vercel

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8000"; // Update if deployed
+const BASE_URL = "https://shl-backend-rtjg.onrender.com"; // Update if deployed
 
 export const fetchRecommendations = async (query, url) => {
   return axios.post(`${BASE_URL}/recommend`, {
