b2aba9191950f4013b30a6cdd2fb7ea7ed553799
sanathkumarr <ravadagundisanath@gmail.com>
2025-04-21 01:54:46 +0530

    third

--- src/api.js
+++ src/api.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = "https://shl-frontend-delta.vercel.app/"; // Update if deployed
+const BASE_URL = "https://shl-backend-rtjg.onrender.com";
 
-export const fetchRecommendations = async (query, url) => {
+export const fetchRecommendations = async (query) => {
   return axios.post(`${BASE_URL}/recommend`, {
     query,
-    url,
   });
 };
