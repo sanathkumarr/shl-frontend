0838d1657428c5b816c73928af96f4056fbb97f1
sanathkumarr <ravadagundisanath@gmail.com>
2025-05-01 19:48:18 +0530

    backend url change

--- src/api.js
+++ src/api.js
@@ -1,10 +1,6 @@
 import axios from "axios";
 
-<<<<<<< HEAD
-const BASE_URL = "https://shl-backend-rtjg.onrender.com";
-=======
 const BASE_URL = "https://shl-backend-production.up.railway.app";
->>>>>>> d9a4a7d (Initial commit with frontend, backend, and scraper)
 
 export const fetchRecommendations = async (query) => {
   return axios.post(`${BASE_URL}/recommend`, {
